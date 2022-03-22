52c7929514755b69e04d0be9457d3714f9323f30
Stephan Klinger <staeff@arcor.de>
2022-03-22 21:05:44 +0100

    Format app.py

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ import pickle
 import os
 
 app = Flask(__name__)
-model = pickle.load(open('data/model.pkl', 'rb'))
+model = pickle.load(open("data/model.pkl", "rb"))
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
 
     int_features = [int(x) for x in request.form.values()]
@@ -19,9 +21,12 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Sales should be $ {}'.format(output))
+    return render_template(
+        "index.html", prediction_text=f"Sales should be $ {output}"
+    )
 
-@app.route('/results',methods=['POST'])
+
+@app.route("/results", methods=["POST"])
 def results():
 
     data = request.get_json(force=True)
@@ -29,6 +34,7 @@ def results():
 
     output = prediction[0]
     return jsonify(output)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
